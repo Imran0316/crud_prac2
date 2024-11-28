ac694ca1ddff44ae7d149878f6178743f58aeee3
Imran <ik775239@gmail.com>
2024-11-28 10:27:40 +0000

    add

---
+++ form.php
@@ -0,0 +1,24 @@
+<form action="" method="post">
+    <label for="name">Name</label> <br>
+    <input type="text" name="name" > <br>
+
+    <label for="email">Email</label> <br>
+    <input type="email" name="email" > <br>
+
+    <label for="password">Password</label> <br>
+    <input type="password" name="password" > <br>
+
+    <label for="phone">Phone</label> <br>
+    <input type="tel" name="phone" > <br>
+
+    <label for="address">Address</label> <br>
+    <input type="text" name="address" > <br>
+
+    <input type="submit" value="submit" name="submit">
+</form>
+
+<?php
+if(isset($_POST["submit"])){
+    
+}
+?>
